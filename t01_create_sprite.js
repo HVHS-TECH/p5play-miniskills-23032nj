b580d1b41b4f3e72c3ce8ad7b178a6eb19d33c50
23032nj <23032nj@hvhs.school.nz>
2026-02-20 09:59:34 +1300

    19/02 save

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -10,7 +10,44 @@
 function setup() {
 console.log("setup: ");
 
-//cnv = new Canvas(800, 800);
+cnv = new Canvas(800, 800);
+
+//horizontal
+//for (var i = 0; i < 10; i++) {
+//  var block = new Sprite(i*80 + 40, 40, 50, 50);
+//  block.color = color(255, 0, 0); // Red color
+//}
+
+//vertical
+//for (var i = 0; i < 10; i++) {
+//  var block = new Sprite(40, i*80 + 40, 50, 50);
+//  block.color = color(255, 0, 0); // Red color
+//}
+
+
+//diagonal
+//for (var i = 0; i < 10; i++) {
+//  var block = new Sprite(i*80 + 40, i*80 + 40, 50, 50);
+// block.color = color(255, 0, 0); // Red color
+//}
+
+//stack
+//for (var i = 0; i < 10; i++) {
+//  var block = new Sprite(400, i*80 + 40, i*75 - 20, 50);
+//  block.color = color(255, 0, 0); // Red color
+//}
+
+for (var row = 0; row < 10; row++) {
+for (var i = 0; i < 10; i++) {
+  var block = new Sprite(i*80 + 40, row*80, 50, 50);
+  block.color = color(255, 0, 0); 
+}
+}
+//for (var i = 0; i < 10; i++) {
+//  var block = new Sprite(i*80 + 40, 120, 50, 50);
+// block.color = color(255, 0, 0);   
+//}
+
 
 //milo = new Sprite(300, 400, 45, 75, '5');
 //milo.color = '#a7cebc';
@@ -19,27 +56,20 @@ console.log("setup: ");
 //vader.color = '#698fe7';
 
 // Now the width and height of the canvas keep changing
-width = random(300,600);
-height = random(300,600);
-
+//width = random(300,600);
+//height = random(300,600);
 // and the sprites keep changing too!
-spriteX= random(50,150);
-spriteY=  random(50,150);  
-
-cnv = new Canvas(width, height);
-
-red = new Sprite(50, 50 , spriteX, spriteY);
-red.color = 'red';
-
-green = new Sprite( width - spriteX, 50 , spriteX, spriteY);
-green.color = 'green';
-
-blue = new Sprite( width - spriteX, height - spriteY , spriteX, spriteY);
-blue.color = 'blue';
-
-yellow = new Sprite( 50, height - spriteY ,  spriteX, spriteY);
-yellow.color = 'yellow';
-
+//spriteX= random(50,150);
+//spriteY=  random(50,150);  
+//cnv = new Canvas(width, height);
+//red = new Sprite(50, 50 , spriteX, spriteY);
+//red.color = 'red';
+//green = new Sprite( width - spriteX, 50 , spriteX, spriteY);
+//green.color = 'green';
+//blue = new Sprite( width - spriteX, height - spriteY , spriteX, spriteY);
+//blue.color = 'blue';
+//yellow = new Sprite( 50, height - spriteY ,  spriteX, spriteY);
+//yellow.color = 'yellow';
 //purple = new Sprite( __, __ ,  spriteX, spriteY);
 //purple.color = 'purple';
 
@@ -48,7 +78,7 @@ yellow.color = 'yellow';
 // draw()
 /*******************************************************/
 function draw() {		
-background ('#f2e0f9');
+background ('#9cbef1');
 }
 
 /*******************************************************/
